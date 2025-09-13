cfc2b89ef85f2a10fc31d8b69b21a971be9783b1
novando <rizkynovando@gmail.com>
2025-09-13 19:27:01 +0700

    feature: config file

--- src/service/lesson/get-lessons-by-user-id.service.ts
+++ src/service/lesson/get-lessons-by-user-id.service.ts
@@ -5,5 +5,6 @@ const validation = Joi.number().min(1).required();
 
 export async function getLessonPaginationByUserId(this: LessonService, userId: number) {
   await validation.validateAsync(userId)
+
   return {success: true}
 }
